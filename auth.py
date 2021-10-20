84dd432f9aa250008810e70e7361159dd45f307f
Américo Júnior <americo@iMac-de-Americo.local>
2021-10-20 21:34:27 +0200

    Front-end done

--- auth.py
+++ auth.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import User, Task
+from models import User, Comment
 from flask_login import login_user
 from app import db
 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
--- main.py
+++ main.py
@@ -14,3 +14,15 @@ def index():
 @main.route('/product/<id>')
 def product(id):
     return render_template('product.html')
+
+@main.route('/product/add', methods=['GET', 'POST'])
+def add_product():
+    return render_template('add-product.html')
+
+@main.route('/account', methods=['GET', 'POST'])
+def account():
+    return render_template('account.html')
+
+@main.route('/profile')
+def profile():
+    return render_template('profile.html')
