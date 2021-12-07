2311322515aa33ba61cf2abe9574f7f9e23a1783
Américo Júnior <62674418+americo@users.noreply.github.com>
2021-12-08 00:50:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,7 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__, static_folder='static', static_url_path='')
-
-    app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'true'
-
-    db.init_app(app)
+    from database import app, db
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
